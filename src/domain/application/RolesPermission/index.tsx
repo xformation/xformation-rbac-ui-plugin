18e954d168a7d0b579df5283ecb6a2aac187c853
manojkrjoshi <manojkr.joshi@gmail.com>
2021-07-22 15:19:19 +0530

    Issues with lable permissions fixed

--- src/domain/application/RolesPermission/index.tsx
+++ src/domain/application/RolesPermission/index.tsx
@@ -46,28 +46,28 @@ export default class RolesPermission extends React.Component<RolePermProps, any>
         return (
             <section className="tab-container row vertical-tab-container">
                 <Nav tabs className="pl-3 pl-3 mb-4 mt-4 col-sm-2">
-                    <Rbac parentName={config.PARENT_NAME} childName="rolepermission-index-permissions">
+                    <Rbac parentName={'Permissions'} childName={'Permissions'}>
                         <NavItem  className="cursor-pointer">
                             <NavLink  className={`tabStyle vertical-nav-link tabStyle ${activeTab === 0 ? 'side-active' : ''}`} onClick={() => { this.toggleTab(0); }} >
                                     Permissions
                             </NavLink>
                         </NavItem>
                     </Rbac>
-                    <Rbac parentName={config.PARENT_NAME} childName="rolepermission-index-roles">
+                    <Rbac parentName={'Roles'} childName={'Roles'}>
                         <NavItem className="cursor-pointer">
                             <NavLink className={`tabStyle vertical-nav-link ${activeTab === 1 ? 'side-active' : ''}`} onClick={() => { this.toggleTab(1); }} >
                                 Roles
                             </NavLink>
                         </NavItem>
                     </Rbac>
-                    <Rbac parentName={config.PARENT_NAME} childName="rolepermission-index-groups">
+                    <Rbac parentName={'Groups'} childName={'Groups'}>
                         <NavItem className="cursor-pointer">
                             <NavLink className={`tabStyle vertical-nav-link ${activeTab === 2 ? 'side-active' : ''}`} onClick={() => { this.toggleTab(2); }} >
                                 Groups
                             </NavLink>
                         </NavItem>
                     </Rbac>
-                    <Rbac parentName={config.PARENT_NAME} childName="rolepermission-index-users">
+                    <Rbac parentName={'Users'} childName={'Users'}>
                         <NavItem className="cursor-pointer">
                             <NavLink className={`tabStyle vertical-nav-link ${activeTab === 3 ? 'side-active' : ''}`} onClick={() => { this.toggleTab(3); }} >
                                 Users
@@ -77,24 +77,24 @@ export default class RolesPermission extends React.Component<RolePermProps, any>
                 </Nav>
 
                 <TabContent activeTab={activeTab} className="col-sm-9 border-left p-t-1">
-                    <Rbac parentName={config.PARENT_NAME} childName="rolepermission-index-permissions">
+                    <Rbac parentName={'Permissions'} childName={'Permissions'}>
                         <TabPane tabId={0}>
                             <Permissions />
                         </TabPane>
                     </Rbac>
-                    <Rbac parentName={config.PARENT_NAME} childName="rolepermission-index-roles">
+                    <Rbac parentName={'Roles'} childName={'Roles'}>
                         <TabPane tabId={1}> 
                             {
                                 activeTab === 1 ? <Roles isLoaded={activeTab === 1 ? "YES" : "NO"}/> : null
                             }
                         </TabPane>
                     </Rbac>
-                    <Rbac parentName={config.PARENT_NAME} childName="rolepermission-index-groups">
+                    <Rbac parentName={'Groups'} childName={'Groups'}>
                         <TabPane tabId={2}>
                             {activeTab === 2 ? <Groups isPageLoaded={activeTab === 2 ? "YES" : "NO"}/> : null}
                         </TabPane>
                     </Rbac>
-                    <Rbac parentName={config.PARENT_NAME} childName="rolepermission-index-users">
+                    <Rbac parentName={'Users'} childName={'Users'}>
                         <TabPane tabId={3}>
                             {activeTab === 3 ? <Users isPageLoaded={activeTab === 3 ? "YES" : "NO"}/> : null}
                         </TabPane>
--- src/domain/application/RolesPermission/rbac/Rbac.tsx
+++ src/domain/application/RolesPermission/rbac/Rbac.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export default class Rbac extends React.Component<any, any>{
+export default class Rbac extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
     this.state = {
@@ -9,50 +9,53 @@ export default class Rbac extends React.Component<any, any>{
       childName: null,
       parentName: null,
       mapPermissions: {},
-      isExternalSecurityEnabled: localStorage.getItem('external_security_enable') === "true" ? true : false,
+      isExternalSecurityEnabled: localStorage.getItem('external_security_enable') === 'true' ? true : false,
     };
     this.getExternaSecurityStatus = this.getExternaSecurityStatus.bind(this);
   }
+
   async componentDidMount() {
     await this.getExternaSecurityStatus();
   }
+
   async getExternaSecurityStatus() {
     // await console.log("external security flag : ", this.state.isExternalSecurityEnabled);
     if (this.state.isExternalSecurityEnabled) {
-      let uInfo = (localStorage.getItem('userInfo') === null || localStorage.getItem('userInfo') === undefined || localStorage.getItem('userInfo') === '') ? '' : localStorage.getItem('userInfo');
-      let userInfo = (uInfo !== '' && uInfo !== null && uInfo !== undefined && uInfo !== "undefined") ? JSON.parse(uInfo) : '';
+      let uInfo = localStorage.getItem('userInfo');
+      // console.log(`String usr : ` + uInfo);
+      let userInfo = uInfo ? JSON.parse(uInfo) : '';
+      // console.log(`Ex User INfo`, userInfo);
       this.setState({
-        mapPermissions: userInfo !== '' ? userInfo.authz.mapPermissions : [],
-        permissions: userInfo !== '' ? userInfo.authz.permissions : [],
+        mapPermissions: userInfo.authz.mapPermissions,
+        permissions: userInfo.authz.permissions,
         childName: this.props.childName,
         parentName: this.props.parentName,
-})
+      });
     }
     // await console.log("permissions : ", this.state.permissions);
   }
 
   include(arr: any, obj: any) {
-    //console.log("parents permissions:  ",arr);
-    return (arr.indexOf(obj) != -1);
+    return arr.indexOf(obj) !== -1;
   }
   checkParent(mapPermissions: any, str: any) {
     return mapPermissions.hasOwnProperty(str);
   }
   render() {
-    const { childName, permissions, mapPermissions, parentName, isExternalSecurityEnabled } = this.state
+    const { childName, mapPermissions, parentName, isExternalSecurityEnabled } = this.state;
     if (!isExternalSecurityEnabled) {
-      // console.log("1. External security disabled. Running with default authentication");
+      // console.log('1. External security disabled. Running with default authentication');
       return this.props.children;
     }
     if (!this.checkParent(mapPermissions, parentName)) {
-      // console.log("2. parent permission not granted. returning null");
-        return null;
+      // console.log(`2. parent permission not granted. returning null, ${mapPermissions}, ${parentName}`);
+      return null;
     }
-      if (!this.include(mapPermissions[parentName], childName)) {
-        // console.log("3. permission not granted. returning null");
-        return null;
-      }
-    // console.log("4. permission granted. returning children");
+    if (!this.include(mapPermissions[parentName], childName)) {
+      // console.log(`3. permission not granted. returning null`);
+      return null;
+    }
+    // console.log(`4. permission granted. returning children`);
     return this.props.children;
   }
 }
